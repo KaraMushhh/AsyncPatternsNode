311ed82e49a4ddc3fb59b925b65ac8f25eef87f7
System Administrator <root@CARMAT-MAC.local>
2023-09-25 15:58:56 +0200

    my firts commit

---
+++ promises.js
@@ -0,0 +1,25 @@
+/*function doAsyncWork(resolve, reject) {
+    //permorm async calls
+    if(success) {
+        resolve(data);
+    }
+    else {
+        reject(reason)
+    }
+}
+
+let myPromise = new Promise((resolve, reject) =>{
+    //permorm async calls
+    if(success) {
+        resolve(data);
+    }
+    else {
+        reject(reason)
+    }
+}
+);
+
+methodThatReturnPromise()
+ .then(data => console.log(data))
+ catch(error => console.log(error))
+ .finally(() => console.log('alld done')); */
